e28b7bc200baf8ac3c7a73e0f44d1f55652c69e7
Wasim Anabtawi <96519449+WasimAnabtawi@users.noreply.github.com>
2024-01-23 09:23:05 +0200

    Delete lib directory

--- lib/Entities/CaregoryLabResults.dart
+++
@@ -1,7 +0,0 @@
-import 'package:soul/Entities/orders.dart';
-
-class CaregoryLabResults {
-  String? CategoryName;
-  List<Order>? OrdersList = [];
-  CaregoryLabResults({this.CategoryName, this.OrdersList});
-}
